d622a1c81146f9d15475868b2daf8cb5385bdaa2
Douglas Leal <douglas.leal@live.com>
2023-02-22 09:45:47 -0300

    Classe Funcionario criada

--- ByteBank_Adm/Models/Funcionarios/Funcionario.cs
+++ ByteBank_Adm/Models/Funcionarios/Funcionario.cs
@@ -8,5 +8,13 @@ namespace ByteBank_Adm.Models.Funcionarios
 {
     public class Funcionario
     {
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.1;
+        }
     }
 }
--- ByteBank_Adm/Program.cs
+++ ByteBank_Adm/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ByteBank_Adm.Models.Funcionarios;
+
+var f = new Funcionario();
+f.Salario = 1500;
+
+Console.WriteLine(f.GetBonificacao());
